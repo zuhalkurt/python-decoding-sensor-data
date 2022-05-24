72dc29dbb5fad3c5c8d4886964efb407442af4ee
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-24 10:58:44 +0100

    Made changews on module2-4

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -20,6 +20,6 @@ class HouseInfo:
         field_data = []
         for record in self.data:
             if rec_date.strftime("%m/%d/%y") == record['date']:
-                field_data.append(record.field)
+                field_data.append(record[field])
         return field_data
 
