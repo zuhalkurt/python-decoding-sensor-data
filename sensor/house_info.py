4a276ff17af0be78e6a39b09a256d022c6fe0f1c
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-24 10:46:46 +0100

    Complate module 2

---
+++ sensor/house_info.py
@@ -0,0 +1,25 @@
+from datetime import date
+from datetime import datetime
+
+today = date.today()
+
+class HouseInfo:
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area=0):
+        field_data = []
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record[field])
+            elif rec_area == record['area']:
+                field_data.append(record[field])
+        return field_data
+
+    def get_data_by_date(self, field, rec_date=today):
+        field_data = []
+        for record in self.data:
+            if date == rec_date:
+                field_data.append(record.field)
+        return field_data
+
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,8 @@
 # Runner script for all modules
 from load_data import load_sensor_data
-
+from house_info import HouseInfo
+from datetime import date
+from datetime import datetime
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -14,6 +16,14 @@ data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 
 # Module 2 code here:
+
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area('id', rec_area=test_area)
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+test_date = datetime.strftime("5/9/20", "%m/%d/%y")
+recs = house_info.get_data_by_area("id", rec_date=test_date)
+print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
 
