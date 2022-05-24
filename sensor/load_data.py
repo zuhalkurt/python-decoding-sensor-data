9922b6442e1587ab667739ac556d6549c06beee7
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-24 09:50:45 +0100

    Complate module1

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -9,7 +9,7 @@ def load_sensor_data():
     for sensor_file in sensor_files:
         with open(sensor_file, 'r') as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
-            for row in data_file:
+            for row in data_reader:
                 sensor_data.append(row)
 
     return sensor_data
