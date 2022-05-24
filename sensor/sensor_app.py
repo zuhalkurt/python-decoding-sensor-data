e94550f1bef907fa06667e1a81200f2444498601
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-24 09:48:58 +0100

    Complate module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,5 @@
 # Runner script for all modules
-
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
@@ -9,6 +9,9 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
+
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 
 # Module 2 code here:
 
