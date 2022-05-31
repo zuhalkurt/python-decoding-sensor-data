ce379922f9954a644a2e500ee9acf87e526f3d25
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-31 11:03:24 +0100

    Made changes on module 5 and finished first 3

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -6,3 +6,7 @@ class HumidityData(HouseInfo):
         for rec in data:
             recs.append(float(rec)*100)
         return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("humidity", rec_area)
+        return self._convert_data(recs)
