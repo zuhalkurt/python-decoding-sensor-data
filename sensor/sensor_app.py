9d294a35b3ed8102abc4d62d254743b3eb34ba82
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-31 10:53:13 +0100

    Made changes on module 4 and finished first 9

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -32,7 +32,9 @@ temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
-
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 # Module 4 code here:
 
 # Module 5 code here:
