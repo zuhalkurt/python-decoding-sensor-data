9bb42740803530848607473479802ea7293570c3
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-31 10:38:31 +0100

    Made changes on module 3 and finished first 4

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -7,5 +7,5 @@ class TemperatureData(HouseInfo):
             recs.append(int(rec, base=10))
         return recs
 
-    def get_data_by_area(self, field, rec_area=0):
+    def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("temperature", rec_area)
