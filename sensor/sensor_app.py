cf48e81a8b6dcfea2fcdaf6cc14cc56c378ef141
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-31 10:49:46 +0100

    Made changes on module 4 and finished first 8

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,6 +3,7 @@ from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import date
 from datetime import datetime
+from temperature_info import TemperatureData
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -26,6 +27,11 @@ recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("House sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
+
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 4 code here:
 
