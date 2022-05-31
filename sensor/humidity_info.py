a555772e5e0f684ad24c9eceb7c14748269c7ab9
Zuhal Kurt <79058038+zuhalkurt@users.noreply.github.com>
2022-05-31 11:07:34 +0100

    Made changes on module 5 and finished first 4

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -1,4 +1,5 @@
 from house_info import HouseInfo
+from datetime import date
 
 class HumidityData(HouseInfo):
     def _convert_data(self, data):
@@ -9,4 +10,8 @@ class HumidityData(HouseInfo):
 
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("humidity", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("humidity", rec_date)
         return self._convert_data(recs)
